ee246689061523253ed5ea93ec6baafabbd49e3f
Muhdnur Shuaib <nur3dtech@gmail.com>
2020-07-12 14:02:12 -0600

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+$(document).ready(function () {
+  var accordionOpen = $(".first_depth p"),
+    secondDepth = $(".second_depth");
+
+  accordionOpen.on("click", function () {
+    accordionOpen.closest(".first_depth").removeClass("on");
+    $(this).closest(".first_depth").addClass("on");
+  });
+});
